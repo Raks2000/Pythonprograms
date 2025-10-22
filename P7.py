1632b0670e73aa3cc4d6a3de4770963905142ece
RakshithM <rakshithaparna886@gmail.com>
2025-10-22 11:48:33 +0530

    Age Group Classifier

---
+++ P7.py
@@ -0,0 +1,12 @@
+# Age Group Classifier
+
+age = int(input("Enter your age: "))
+
+if age < 13:
+    print("You are a Child.")
+elif age < 20:
+    print("You are a Teenager.")
+elif age < 60:
+    print("You are an Adult.")
+else:
+    print("You are a Senior Citizen.")
