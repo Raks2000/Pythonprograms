036effac00e2043fc3f6bdb23c05d30fabb968b1
Raks2000 <100326440+Raks2000@users.noreply.github.com>
2025-10-29 09:51:45 +0530

    Delete dictionary.py

--- dictionary.py
+++
@@ -1,8 +0,0 @@
-with open("stocks_sample.csv", "r") as file:
-    dictionary = {}
-    for line in file:
-        data = line.strip().split(",")
-        key = data[0]
-        values = data[1:]
-        dictionary[key] = values
-    print(dictionary)
